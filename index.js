4d4de6e78c3561a81c3a9e3b06c8b47033a91e84
Natalia Silva <nataliadasilva.dev@gmail.com>
2019-12-19 12:57:02 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,31 @@
+var app = require('express')()
+var http = require('http').createServer(app)
+var io = require('socket.io')(http)
+
+//Rota principal
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html')
+})
+
+//Conexão com o socket
+io.on('connection', function(socket){
+	console.log("Um usuário se conectou")
+
+	socket.broadcast.emit('Olá!')
+
+	//Evento chamado quando o usuário fecha a página
+	socket.on('disconnect', function(){
+		console.log("Um usuário se desconectou")
+	})
+
+	//Recebendo mensagem
+	socket.on('message', function(txt){
+		console.log("Disseram: " + txt)
+		io.emit('message', txt)
+	})
+});
+
+
+http.listen(3000, function(){
+	console.log("Projeto rodando em http://localhost:3000")
+})
